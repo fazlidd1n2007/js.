6c302a0e139e3565421393e6d56fb020072c8409
Fazlidd1n2007 <muhammadiyevfazliddin15@gmail.com>
2023-11-09 20:13:05 +0500

    kmfkrek

---
+++ main/main.js
@@ -0,0 +1,47 @@
+var Ism =String(prompt("Ism yozing"));
+console.log(Ism);
+
+var Familya =String(prompt("Familya yozing"));
+console.log(Familya);
+
+var Yosh =Number(prompt("Yosh yozing"));
+console.log(Yosh);
+
+var Tel =Number(prompt("Tel yozing"));
+console.log(Tel);
+
+var Manzil =String(prompt("Manzil yozing"));
+console.log(Manzil);
+
+var Qiziqish =String(prompt("Qiziqish yozing"));
+console.log(Qiziqish);
+
+var Yil =String(prompt("Yil yozing"));
+console.log(Yil);
+
+var Ustozi =String(prompt("Ustozini yozing"));
+console.log(Ustozi);
+
+var Millati =String(prompt("Millati yozing"));
+console.log(Millati);
+
+var Maktabi =String(prompt("Maktabini yozing"));
+console.log(Maktabi);
+
+var Javoblar = [
+    "Ism" + " " + Ism + "/n" +
+    "Familya" + " " + Familya + "/n"+
+    "Yosh" + " " + Yosh + "/n"+
+    "Tel" + " " + Tel + "/n"+
+    "Manzil" + " " + Manzil + "/n"+
+    "Qiziqish" + " " + Qiziqish + "/n"+
+    "Yil" + " " + Yil + "/n"+
+    "Ustozini" + " " + Ustozi + "/n"+
+    "Millati" + " " + Millati + "/n"+
+    "Maktabini" + " " + Maktabi + "/n"
+]
+
+alert(Javoblar);
+
+
+
